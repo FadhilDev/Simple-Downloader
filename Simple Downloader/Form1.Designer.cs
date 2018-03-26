d4ec0c6583be9d257d56b1e8370655513903b117
Fadhil <fadhilabbash@gmail.com>
2018-03-26 16:53:25 +0300

    first commit

---
+++ Simple Downloader/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Simple_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.prgDownload = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(12, 12);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(460, 20);
+            this.txtUrl.TabIndex = 0;
+            this.txtUrl.TextChanged += new System.EventHandler(this.txtUrl_TextChanged);
+            // 
+            // prgDownload
+            // 
+            this.prgDownload.Location = new System.Drawing.Point(15, 51);
+            this.prgDownload.Name = "prgDownload";
+            this.prgDownload.Size = new System.Drawing.Size(457, 8);
+            this.prgDownload.TabIndex = 1;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 35);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(157, 13);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Status: waiting for download...";
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(12, 64);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(215, 13);
+            this.lblProgress.TabIndex = 3;
+            this.lblProgress.Text = "0.00 MB (0 bytes) of 0.00 MB (0 bytes) 0%";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Enabled = false;
+            this.btnCancel.Location = new System.Drawing.Point(12, 105);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(85, 26);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Enabled = false;
+            this.btnDownload.Location = new System.Drawing.Point(387, 105);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(85, 26);
+            this.btnDownload.TabIndex = 5;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 142);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.prgDownload);
+            this.Controls.Add(this.txtUrl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mini Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.ProgressBar prgDownload;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnDownload;
+    }
+}
+
---
+++ Simple Downloader/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace Simple_Downloader
+{
+    public partial class Form1 : Form
+    {
+        // Create the file name string.
+        string filename;
+        // Create the webclient.
+        WebClient webClient = new WebClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Create the download void.
+        private void DownloadFile(string url, string save)
+        {
+            // We need to add an using, so we can download another file after the first one completes. If we don't that it will not work.
+
+            // Run code every time the download changes.
+            webClient.DownloadProgressChanged += webClient_DownloadProgressChanged;
+            // Run codes when file download has been completed.
+            webClient.DownloadFileCompleted += webClient_DownloadFileCompleted;
+            // Start the download.
+            webClient.DownloadFileAsync(new Uri(url), save);
+            
+        }
+        private void webClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Update the progress bar.
+            prgDownload.Value = e.ProgressPercentage;
+            // Update status label.
+            lblStatus.Text = "Status: downloading...";
+            // Update progress label.
+            //lblProgress.Text = "Progress: downloaded " + (e.BytesReceived / 1024d / 1024d).ToString("0.00 MB") + " (" + e.BytesReceived + " bytes) of " + (e.TotalBytesToReceive/1024d/1024d).ToString("0.00 MB") + " (" + e.TotalBytesToReceive + " bytes) " + e.ProgressPercentage + "%";
+            lblProgress.Text =(e.BytesReceived / 1024d / 1024d).ToString("0.00 MB") + " of " + (e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00 MB  ")+ e.ProgressPercentage + "%";
+            // Disable Download button.
+            btnDownload.Enabled = false;
+            // Enable Cancel button.
+            btnCancel.Enabled = true;
+            // Make the textbox readonly.
+            txtUrl.ReadOnly = true;
+        }
+
+        private void webClient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            //Run download canceled codes.
+                if (e.Cancelled == true)
+            {
+                lblStatus.Text = "Status: download canceled.";
+            }
+            // Run download completed codes.
+            else if (e.Cancelled == false)
+            {
+                lblStatus.Text = "Status: download completed!";
+                MessageBox.Show("Download completed!", "Completed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            // Enable Download button.
+            btnDownload.Enabled = false;
+            // Disabled Cancel button.
+            btnCancel.Enabled = false;
+            // Reset the progress bar.
+            prgDownload.Value = 0;
+            // Make the textbox writeable.
+            txtUrl.ReadOnly = false;
+            txtUrl.Text = string.Empty;
+        }
+
+
+     
+
+        private void txtUrl_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // Now we are going to create the function, that enter the file name automatically in the savefiledialog.
+                Uri uri = new Uri(txtUrl.Text);
+                // Save the file name to the string.
+                filename = Path.GetFileName(uri.LocalPath);
+                btnDownload.Enabled = true;
+            }
+            catch(Exception ex)
+            {
+                // Leave this blank, we don't need an exception message.
+            }
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            // Check if the user has entered an url address.
+            if (txtUrl.Text == string.Empty)
+            {
+                // Show error message if messagebox is empty.
+                MessageBox.Show("Please enter an url address, before clicking on Download.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    // Create a savefiledialog so the user can save the file.
+                    SaveFileDialog dialog = new SaveFileDialog();
+                    // Select the file type as all files.
+                    dialog.Filter = "All Files|*.*";
+                    // Write the file name for the user.
+                    dialog.FileName = filename;
+                    // Open the dialog with dialogresult.
+                    DialogResult result = dialog.ShowDialog();
+                    // Check if the user has clicked OK.
+                    if (result == DialogResult.OK)
+                    {
+                        // Start the download.
+                        DownloadFile(txtUrl.Text, dialog.FileName);
+                    }
+
+                }
+                catch
+                {
+                    // Leave this blank, we don't need an exception message.
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // Cancel the download.
+            webClient.CancelAsync();
+        }
+
+
+
+
+
+
+
+
+        //static void Main(string[] args)
+        //{
+        //    var fw = new FileDownload("http://download.microsoft.com/download/E/E/2/EE2D29A1-2D5C-463C-B7F1-40E4170F5E2C/KinectSDK-v1.0-Setup.exe", @"D:\KinetSDK.exe", 5120);
+
+        //    // Display progress...
+        //    Task.Factory.StartNew(() =>
+        //    {
+        //        while (!fw.Done)
+        //        {
+        //            Console.SetCursorPosition(0, Console.CursorTop);
+        //            Console.Write(string.Format("ContentLength: {0} | BytesWritten: {1}", fw.ContentLength, fw.BytesWritten));
+        //        }
+        //    });
+
+        //    // Start the download...
+        //    fw.Start();
+
+        //    // Simulate pause...
+        //    Thread.Sleep(500);
+        //    fw.Pause();
+        //    Thread.Sleep(2000);
+
+        //    // Start the download from where we left, and when done print to console.
+        //    fw.Start().ContinueWith(t => Console.WriteLine("Done"));
+
+        //    Console.ReadKey();
+        //}
+        //public void Downloade()
+        //{
+        //    var fw = new FileDownload("http://download.microsoft.com/download/E/E/2/EE2D29A1-2D5C-463C-B7F1-40E4170F5E2C/KinectSDK-v1.0-Setup.exe", @"D:\KinetSDK.exe", 5120);
+
+        //    // Display progress...
+        //    Task.Factory.StartNew(() =>
+        //    {
+        //        while (!fw.Done)
+        //        {
+        //            Console.SetCursorPosition(0, Console.CursorTop);
+        //            Console.Write(string.Format("ContentLength: {0} | BytesWritten: {1}", fw.ContentLength, fw.BytesWritten));
+        //        }
+        //    });
+
+        //    // Start the download...
+        //    fw.Start();
+
+        //    fw.Pause();
+           
+
+        //    // Start the download from where we left, and when done print to console.
+        //    fw.Start().ContinueWith(t => Console.WriteLine("Done"));
+
+        //    Console.ReadKey();
+        //}
+
+
+    }
+}
